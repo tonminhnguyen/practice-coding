508bb5da2c207ac1d2deb04f7dbdf169d75526c4
tonminhnguyen <minh.nguyen@vietlink.jp>
2025-01-17 16:19:51 +0700

    final1

--- 2/fibonacci.cpp
+++ 2/fibonacci.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
 
-int main*()
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    int prev1 = 1, prev2 = 0;
+    int curr = 0;
+    for (int i = 2; i <= n; i++) {
+        curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return curr;
+}
+
+int main() {
+    int n = 5;
+    std::cout << fibonacci(n) << std::endl;
+    return 0;
+}
